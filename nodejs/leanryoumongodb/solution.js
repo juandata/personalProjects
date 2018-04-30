fa5ebe86c4b21f6ddf7361f0cee1731f0ed9130f
Juan David Tabares Arce <jtabares@nisum.com>
2018-04-30 18:30:59 -0300

    updating from dell

--- nodejs/leanryoumongodb/solution.js
+++ nodejs/leanryoumongodb/solution.js
@@ -29,12 +29,12 @@ MongoClient.connect(url, function(err, db) {
 
 // 2.1 Insert the elements of the array into the database collection:
 /*
-MongoClient.connect(url + 'learnyoumongo', function(err, database) {
+MongoClient.connect(url, function(err, database) {
      // db gives access to the database
      var theDb = database.db('learnyoumongo');
      //iterate over the array of objects and insert them into the collection
   jsonusers.users.map((el) => {
-     theDb.collection('users').insertOne(el, function(err, res) {
+     theDb.collection('parrots').insertOne(el, function(err, res) {
        if (err) throw err;
        console.log("1 document inserted");
      });
@@ -51,21 +51,77 @@ MongoClient.connect(url + 'learnyoumongo', function(err, database) {
     });
       database.close();
    });
-   */
+*/
 
-   // 3 find
+   // 3 and 4  find project
+   /*
 var age = process.argv[2]
 MongoClient.connect(url, function(err, database) {
   if (err) throw err;
   var theDb = database.db('learnyoumongo');
   var parrots = theDb.collection('parrots');
   parrots.find({
-    age: {
-      $gt: +age
-    }
-  }).toArray(function(err, docs) {
+    age : {
+    $gt : +age
+  }
+}).project({_id : 0}).toArray(function(err, docs) {
     if (err) throw err
     console.log(docs)
     database.close()
   })
 })
+*/
+
+//5 Insert
+/*
+var firstName = process.argv[2], lastName = process.argv[3];
+var doc = {
+firstName: firstName
+, lastName: lastName
+};
+MongoClient.connect(url, function(err, database){
+  if (err) throw err;
+  var theDB = database.db('learnyoumongo');
+  var collection = theDB.collection('docs');
+  collection.insert(doc, function(err, data) {
+    if (err) throw err
+    console.log(JSON.stringify(doc));
+    db.close();
+  })
+});
+*/
+
+//6 Update
+
+var newValues = {}, username = process.argv[2];
+MongoClient.connect(url, function(err, database){
+  if (err) throw err;
+  var theDB = database.db(process.argv[2]), collection = theDB.collection('users'), username = process.argv[2];
+  //update username property to every every doc in the collection
+  /*
+  jsonusers.users.map((el)=> {
+    collection.updateMany({name : el.name},
+      {
+        $set : {
+          username : el.name + '_user'
+        }
+      }
+
+      , function(err, res){
+      if (err) throw err;
+      console.log( el.name + " updated");
+    });
+  });
+    database.close();
+});
+*/
+//update just one document property from the Collection
+collection.update({username : 'tinatime'}, {
+  $set : {
+  age : 40
+}
+},function(err, resp){
+  console.log("updated");
+  database.close();
+});
+});
